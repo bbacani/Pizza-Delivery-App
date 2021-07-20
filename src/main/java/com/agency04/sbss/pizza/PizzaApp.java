fb547fc2f68c3a8b1002992cf2bdd7ebf7f7287a
bacani <bernard.bacani@gmail.com>
2021-07-20 19:29:55 +0200

    Update

--- src/main/java/com/agency04/sbss/pizza/PizzaApp.java
+++ src/main/java/com/agency04/sbss/pizza/PizzaApp.java
@@ -1,9 +1,13 @@
 package com.agency04.sbss.pizza;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class PizzaApp {
 
-    public static void main(String[] args) {
-        // Add some pizza code
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaApp.class, args);
+	}
 
 }
