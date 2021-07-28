7cde013e4ce35f35055d2d1e5683da66d3d46a6f
bacani <bernard.bacani@gmail.com>
2021-07-28 23:59:44 +0200

    Solved week-2 part 1

--- src/main/java/com/agency04/sbss/pizza/service/PizzaDeliveryService.java
+++ src/main/java/com/agency04/sbss/pizza/service/PizzaDeliveryService.java
@@ -5,27 +5,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 public class PizzaDeliveryService {
 
     private PizzeriaService pizzeriaService;
 
     public PizzaDeliveryService() {
-        System.out.println("PizzaDeliveryService: inside no-arg constructor");
+        System.out.println(">> PizzaDeliveryService: inside no-arg constructor");
     }
 
     public PizzaDeliveryService(PizzeriaService pizzeriaService) {
-        System.out.println("PizzaDeliveryService: inside arg constructor");
+        System.out.println(">> PizzaDeliveryService: inside arg constructor");
         this.pizzeriaService = pizzeriaService;
+    }
+
+    @PostConstruct
+    public void doMyStartup() {
+        System.out.println(">> PizzaDeliveryService: inside of doMyStartup()");
+    }
+
+    @PreDestroy
+    public void doMyCleanup() {
+        System.out.println(">> PizzaDeliveryService: inside of doMyCleanup()");
     }
 
     @Autowired
     public void setPizzeriaService(@Qualifier("marzia") PizzeriaService pizzeriaService) {
-        System.out.println("PizzaDeliveryService: setter method - setPizzeriaService");
+        System.out.println(">> PizzaDeliveryService: setter method - setPizzeriaService");
         this.pizzeriaService = pizzeriaService;
     }
 
     public String orderPizza(Pizza pizza) {
-        return "Name: " + pizza.getName() + "Ingredients: " + pizza.getIngredients();
+        return "Name: " + pizza.getName() + "\n" +
+                "Ingredients: " + pizza.getIngredients() + "\n";
     }
 }
